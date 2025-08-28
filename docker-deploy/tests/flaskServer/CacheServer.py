0055a0dd76883edaea8f58f210226967721834d4
yyxff <salmontwosday@gmail.com>
2025-08-28 23:47:39 +0800

    Updated swarm test

--- docker-deploy/tests/flaskServer/CacheServer.py
+++ docker-deploy/tests/flaskServer/CacheServer.py
@@ -161,4 +161,4 @@ def cache_response_no_tore():
 
 # start server
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
